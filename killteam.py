346f5fb6648bbbb3c7f8b79c0ed5011cbe51307e
Syndwar <syndwar@yandex.ru>
2023-04-17 22:57:32 +0300

    kroot team

--- killteam.py
+++ killteam.py
@@ -1,20 +1,36 @@
 #/usr/bin/env python3
 
-import ktdb
+import json
 
-KILL_TEAM_PAGE = """
+FACTIONS_PAGE = """
 <html>
     <head>
         <title>Kill Team 2.0</title>
     </head>
+    <style>
+    table.center {
+      margin-left: auto; 
+      margin-right: auto;
+    }
+    table, td, th {
+        padding: 5px;
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th {
+        text-aign: left;
+    }
+    </style>
     <body>
         <h1 align='center'>Kill Teams</h1>
+        <table class=\"center\">
 {body}
+        </table>
     </body>
 </html>
 """
 
-FIRE_TEAM_PAGE = """
+KILL_TEAMS_PAGE = """
 <html>
     <head>
         <title>Kill Team 2.0 - {header}</title>
@@ -36,32 +52,37 @@ FIRE_TEAM_PAGE = """
     <body>
         <h1 align='center'>{header}</h1>
 {body}
+        <div align='center'><a href=\"main.html\">Back</a></div>
     </body>
 </html>
 """
 
+g_factions = {}
+g_factions_order = []
 g_kill_teams = {}
-g_fire_teams = {}
+g_kill_teams_order = []
 g_units = []
 g_weapons = {}
 
 def tab(n):
     return n*' '
 
-class KillTeam():
-    def __init__(self, data):
-        self.id = data['id']
+class Faction():
+    def __init__(self, id, data):
+        self.id = id
         self.name = data['name']
 
-class FireTeam():
-    def __init__(self, data):
-        self.id = data['id']
+class KillTeam():
+    def __init__(self, id, data):
+        self.id = id
+        self.name = data['name']
+        self.kill_team = data['kill_team']
 
 class Unit():
     def __init__(self, data):
         self.id = data['id']
         self.name = data['name']
-        self.fireteam = data['fireteam']
+        self.kill_team = data['kill_team']
         self.melee = None
         self.ranged = None
         self.addWeapons(data['weapons'])
@@ -85,30 +106,47 @@ class Unit():
         return output
 
 class Weapon():
-    def __init__(self, data):
-        self.id = data['id']
+    def __init__(self, id, data):
+        self.id = id
         self.name = data['name']
         self.type = data['type']
 
-def create_kill_teams():
-    for data in ktdb.KILLTEAMS:
-       kt = KillTeam(data)
-       g_kill_teams[kt.id] = kt
+def create_order():
+    with open('data/order.json') as f:
+        json_data = json.loads(f.read())
+        if json_data:
+            for id in json_data['factions']:
+                g_factions_order.append(id)
+            for id in json_data['kill_teams']:
+                g_kill_teams_order.append(id)
 
-def create_fire_teams():
-    for data in ktdb.FIRETEAMS:
-        ft = FireTeam(data)
-        g_fire_teams[ft.id] = ft
+def create_factions():
+    with open('data/factions.json', 'r') as f:
+        json_data = json.loads(f.read())
+        if json_data:
+            for id in json_data:
+                g_factions[id] = Faction(id, json_data[id])
+
+def create_kill_teams():
+    with open('data/kill_teams.json', 'r') as f:
+        json_data = json.loads(f.read())
+        if json_data:
+            for id in json_data:
+                g_kill_teams[id] = KillTeam(id, json_data[id])
 
 def create_units():
-    for data in ktdb.UNITS:
-        unit = Unit(data)
-        g_units.append(unit)
+    with open('data/units.json', 'r') as f:
+        units_db = json.loads(f.read())
+        if units_db: 
+            for data in units_db: 
+                g_units.append(Unit(data))
 
 def create_weapons():
-    for data in ktdb.WEAPONS:
-        weapon = Weapon(data)
-        g_weapons[weapon.id] = weapon
+    with open('data/weapons.json', 'r') as f:
+        weapons_db = json.loads(f.read())
+        if weapons_db: 
+            for id in weapons_db: 
+                g_weapons[id] = Weapon(id, weapons_db[id])
 
 def serialize():
     create_main_page()
@@ -116,38 +154,44 @@ def serialize():
 
 def create_main_page():
     body = ""
-    for data in ktdb.KILLTEAMS:
-        body = f"{body}{tab(2)}<h2>{data['name']}</h2>\n"
-        for fdata in ktdb.FIRETEAMS:
-            if (data['id'] == fdata['killteam']):
-                body = f"{body}{tab(2)}<h3><a href=\"{fdata['id']}.html\">{fdata['name']}</a></h3>\n"
+    for f_id in g_factions_order:
+        first_row = True
+        faction = g_factions[f_id]
+        for kt_id in g_kill_teams_order:
+            kt = g_kill_teams[kt_id]
+            if (f_id == kt.kill_team):
+                page_url = f"<a href=\"{kt.id}.html\">{kt.name}</a>"
+                if first_row:
+                    body = f"{body}{tab(2)}<tr><th>{faction.name}</th><td>{page_url}</td></tr>\n"
+                else:
+                    body = f"{body}{tab(2)}<tr><td></td><td>{page_url}</td></tr>\n"
+                first_row = False
 
-    page = KILL_TEAM_PAGE.replace('{body}', body)
+    page = FACTIONS_PAGE.replace('{body}', body)
     with open('main.html', 'w') as f:
         f.write(page)
 
 def create_team_pages():
-    for fdata in ktdb.FIRETEAMS:
+    for id in g_kill_teams:
+        kt = g_kill_teams[id]
         body = ""
         for unit in g_units:
             if ("" == body):
                 body = f"{tab(4)}<table class=\"center\">"
-            if (fdata['id'] == unit.fireteam):
+            if (kt.id == unit.kill_team):
                 unit_table = unit.serialize()
                 body = f"{body}{unit_table}\n"
         if ("" != body):
             body = f"{body}{tab(4)}</table>"
 
-        with open(f"{fdata['id']}.html", 'w') as f:
-            page = FIRE_TEAM_PAGE.replace('{body}', body).replace('{header}', fdata['name'])
+        with open(f"{kt.id}.html", 'w') as f:
+            page = KILL_TEAMS_PAGE.replace('{body}', body).replace('{header}', kt.name)
             f.write(page)
 
-def create_unit_tables():
-    pass
-
 def main():
+    create_order()
+    create_factions()
     create_kill_teams()
-    create_fire_teams()
     create_weapons()
     create_units()
     serialize()
